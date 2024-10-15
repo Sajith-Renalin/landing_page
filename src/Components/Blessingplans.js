d433a1c9e58375024baa5f12e02a31699274e136
Sajith-Renalin <kumarsajith274@gmail.com>
2024-10-15 16:45:24 +0530

    added header and footer in the bp page

--- src/Components/Blessingplans.js
+++ src/Components/Blessingplans.js
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import '../Styl.css'; // Import the CSS file
+import "../App.css";
 import youngPartnerImage from '../Assets/yp.jpg';
 import familyBlessingImage from '../Assets/fbp.jpg';
 import businessBlessingImage from '../Assets/bbp.jpg';
 import tvClubImage from '../Assets/tvc.jpg';
 import jobBlessingImage from '../Assets/jbp.jpg';
 import magazineMinistryImage from '../Assets/church.jpg';
+import logo from "../Assets/logo.png";
 
 // Sample data for the plans
 const plans = [
@@ -50,6 +52,26 @@ const plans = [
   },
 ];
 
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={logo} alt="logo" className="logo-image" />
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">About</li>
+          <li className="nav-item">Blessing Plans</li>
+          <li className="nav-item">Prayer Towers</li>
+          <li className="nav-item">Job Opportunities</li>
+          <li className="nav-item">Contact</li>
+          <li className="nav-item">Programs</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 // PlanCard component to display each plan
 const PlanCard = ({ id, imageSrc, title, description }) => {
   return (
@@ -90,11 +112,38 @@ const PlansList = () => {
   );
 };
 
+const Footer =() =>{
+  return(
+    <footer className="footer">
+    <div className="footer-content">
+      <div className="footer-left">
+        <img src={logo} alt="logo" className="logo-image" />
+        <p>
+          Jesus Calls is a global ministry founded by Late Brother D.G.S. Dhinakaran and led by Dr. Paul Dhinakaran. Jesus Calls serves to bring comfort and healing to the broken-hearted without distinction through prayer offered all 24/7 throughout the year.
+        </p>
+      </div>
+      <div className="footer-right">
+        <h2>Want to know more?</h2>
+        <p>24*7 Prayers</p>
+        <p><strong>Phone:</strong> 8546999000</p>
+        <p><strong>For Queries, Call:</strong> 0442345677</p>
+      </div>
+      <div className="footer-right">
+        <h2>Contact Details</h2>
+        <p><strong>Address:</strong> Jesus Calls, 16, Dr. D.G.S Dhinakaran Road, R.A.Puram, Chennai - 600 028.</p>
+        <p><strong>Email:</strong> admin@jesuscalls.org</p>
+      </div>
+    </div>
+  </footer>
+  );
+};
 // Main component for the LogoDetailsPage
 const LogoDetailsPage = () => {
   return (
     <div className="app">
+      <Header />
       <PlansList />
+      <Footer />
     </div>
   );
 };
