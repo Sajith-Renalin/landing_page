288a41c6a9917d1fb4a59905a1309be85b568f2d
Sajith-Renalin <kumarsajith274@gmail.com>
2024-10-17 16:04:43 +0530

    added some changes

--- src/Components/PlanDetailPage.js
+++ src/Components/PlanDetailPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import "../Css/Detail.css";
+import "../Css/Footer.css";
+import logo from "../Assets/logo.png";
 import youngPartnerImage from "../Assets/yp.jpg";
 import familyBlessingImage from "../Assets/fbp.jpg";
 import businessBlessingImage from "../Assets/bbp.jpg";
@@ -56,16 +58,72 @@ Priya's elder sister Jothi lived in Ashok Nagar, Chennai. Her husband Mr. Balaji
   },
 ];
 
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={logo} alt="logo" className="logo-image" />
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">About</li>
+          <li className="nav-item">
+            Blessing Plans
+          </li>
+          <li className="nav-item">Prayer Towers</li>
+          <li className="nav-item">Job Opportunities</li>
+          <li className="nav-item">Contact</li>
+          <li className="nav-item">Programs</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 const PlanDetailPage = () => {
   const { id } = useParams();
   const plan = planDetails.find((p) => p.id === parseInt(id));
 
   return (
     <div className="plan-detail-page">
+      <Header />
       <h1>{plan.title}</h1>
       <img src={plan.imageSrc} alt={plan.title} className="plan-detail-image" />
       <p>{plan.content}</p>
       {/* Here you can add the additional paragraph after the image */}
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="footer-left">
+            <img src={logo} alt="logo" className="logo-image" />
+            <p>
+              Jesus Calls is a global ministry founded by Late Brother D.G.S.
+              Dhinakaran and led by Dr. Paul Dhinakaran. Jesus Calls serves to
+              bring comfort and healing to the broken-hearted without
+              distinction through prayer offered all 24/7 throughout the year.
+            </p>
+          </div>
+          <div className="footer-right">
+            <h2>Want to know more?</h2>
+            <p>24*7 Prayers</p>
+            <p>
+              <strong>Phone:</strong> 8546999000
+            </p>
+            <p>
+              <strong>For Queries, Call:</strong> 0442345677
+            </p>
+          </div>
+          <div className="footer-right">
+            <h2>Contact Details</h2>
+            <p>
+              <strong>Address:</strong> Jesus Calls, 16, Dr. D.G.S Dhinakaran
+              Road, R.A.Puram, Chennai - 600 028.
+            </p>
+            <p>
+              <strong>Email:</strong> admin@jesuscalls.org
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
